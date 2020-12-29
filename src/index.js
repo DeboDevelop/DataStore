8ff727d80dbf3f168de98fd6c2d1076c1e757a2f
DeboDevelop <debajyotidofficial@gmail.com>
2020-12-29 19:20:35 +0530

    Created the createFile method to created files.

--- src/index.js
+++ src/index.js
@@ -20,9 +20,28 @@ class Database {
         }
         try {
             fs.mkdirSync(path.join(this.filePath, "data"));
+            this.filePath = path.join(this.filePath, "data");
         } catch (e) {
             if (e.code == "EEXIST") console.log("Directory already exist! Data will be saved there.");
             else throw e;
+        }
+    }
+    createData(key, value) {
+        if (key.length > 32) {
+            return { errMsg: "Key is more than 32 characters." };
+        }
+        if (Buffer.byteLength(JSON.stringify(value)) > 16 * 1024) {
+            return { errMsg: "Value is more than 16 KB." };
+        }
+        try {
+            if (fs.existsSync(path.join(this.filePath, `${key}.json`))) {
+                return { errMsg: "Key already exist." };
+            } else {
+                fs.writeFileSync(path.join(this.filePath, `${key}.json`), JSON.stringify(value), "utf8");
+                return { successMsg: "File is Created Successfully." };
+            }
+        } catch (e) {
+            return e;
         }
     }
     printName() {
